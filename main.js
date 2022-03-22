917edd753495c41cec62a08abe11d89624e24013
Christine <cenguyen9@gmail.com>
2022-03-22 18:10:37 -0500

    fixed radio input names and arrays; created findMealValue and generateDish functions

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ let mains = [
     "Margarita Pizza"
 ]
 
-let desserts = [
+let dessert = [
     "Apple Pie",
     "Lemon Meringue Pie",
     "Black Forest Cake",
@@ -48,11 +48,34 @@ let desserts = [
     "Eclairs"
 ]
 
-let pickBreakfast = document.querySelector('#breakfast');
+let whichMealOption = document.getElem
 let letsCookBtn = document.querySelector('.lets-cook-btn');
 
 letsCookBtn.addEventListener('click', generateDish);
 
-function generateDish() {
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
 
+function generateDish() {
+    let mealoption = findMealValue();
+    console.log(mealoption);
+    if (mealoption === 'sides') {
+        console.log(sides[getRandomIndex(sides)]);
+    } else if (mealoption === 'main-dish') {
+        console.log(mains[getRandomIndex(mains)]);
+    } else if (mealoption === 'dessert') {
+        console.log(dessert[getRandomIndex(dessert)]);
+    }
+
+}
+
+function findMealValue () {
+    let checkRadio = document.getElementsByName('meal_option');
+
+    for (let i = 0; i < checkRadio.length; i++) {
+        if (checkRadio[i].checked) {
+            return checkRadio[i].value;
+        }
+    }
 }
