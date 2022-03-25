1e82944888de1aaaf5701810fd8c2785d6ed3295
Christine <cenguyen9@gmail.com>
2022-03-25 13:27:38 -0500

    added add a new recipe function; css

--- main.js
+++ main.js
@@ -53,15 +53,28 @@ let entireMealOptions = [];
 //let whichMealOption = document.getElem
 let letsCookBtn = document.querySelector('.lets-cook-btn');
 let mealResults = document.querySelector('.display-meal');
+let addARecipeTopBtn = document.querySelector('.add-recipe-btn');
 let cookPotImg = document.querySelector('.cook-pot-img');
 let clearMealBtn = document.querySelector('.clear-meal-div');
+let mealTypeOption = document.querySelector('#meal-option');
+let recipeNameInput = document.querySelector('#recipe-name');
+let addRecipeForm = document.querySelector('.add-new-recipe-form')
+let addNewRecipeBtn = document.querySelector('.add-new-recipe');
 
 letsCookBtn.addEventListener('click', displayMealOption);
+
+addARecipeTopBtn.addEventListener('click', function() {
+    console.log('is this working');
+    addRecipeForm.classList.toggle('hidden');
+})
+
 clearMealBtn.addEventListener('click', function() {
     cookPotImg.classList.remove('hidden');
     mealResults.classList.add('hidden');
     clearMealBtn.classList.add('hidden');
 })
+
+addNewRecipeBtn.addEventListener('click', addNewRecipe);
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
@@ -111,4 +124,24 @@ function displayMealOption() {
         mealResults.innerHTML = `<article class="make-this"><h3 class="you-should-make">You should make: </h3>
         <h1>${mealOption}!</h1></article>`;
     }
+}
+
+function addNewRecipe() {
+    let mealType = mealTypeOption.value;
+    let recipeNameValue = recipeNameInput.value;
+    let recipeName = recipeNameValue.charAt(0).toUpperCase() + recipeNameValue.slice(1);
+    console.log(mealType, recipeName);
+    if (mealType === 'sides') {
+        sides.push(recipeName);
+    } else if (mealType === 'mains') {
+        mains.push(recipeName);
+    } else if (mealType === 'dessert') {
+        dessert.push(recipeName);
+    }
+    cookPotImg.classList.add('hidden');
+    clearMealBtn.classList.remove('hidden');
+    mealResults.classList.remove('hidden');
+    mealResults.innerHTML = `<article class="add-this"><h3 class="you-added">You added: </h3>
+    <h1>${recipeName} to ${mealType}!</h1></article>`;
+    console.log(mealResults);
 }
