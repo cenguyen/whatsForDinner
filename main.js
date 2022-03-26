3863302cd7e78dc50c261f35050d943f38fab20c
Christine <cenguyen9@gmail.com>
2022-03-26 10:58:59 -0500

    tried to refactor

--- main.js
+++ main.js
@@ -52,52 +52,65 @@ let entireMealOptions = [];
 
 // button variables
 let letsCookBtn = document.querySelector('.lets-cook-btn'); // generate a meal
-let addARecipeTopBtn = document.querySelector('.add-recipe-btn'); // toggles add recipe form
+let showAddRecipeFromBtn = document.querySelector('.add-recipe-btn'); // toggles add recipe form
 let clearMealBtn = document.querySelector('.clear-meal-div'); // clear btn
 let addNewRecipeBtn = document.querySelector('.add-new-recipe'); // adds a recipe (user input)
 
-let mealResults = document.querySelector('.display-meal'); 
-let addRecipeForm = document.querySelector('.add-new-recipe-form');
+let showMealOption = document.querySelector('.display-meal'); 
+let addNewRecipeForm = document.querySelector('.add-new-recipe-form');
 
 let cookPotImg = document.querySelector('.cook-pot-img');
 
-let mealTypeInput = document.querySelector('#meal-option-input');
-let recipeNameInput = document.querySelector('#recipe-name-input');
+let inputMealType = document.querySelector('#meal-option-input');
+let inputRecipeName = document.querySelector('#recipe-name-input');
 
 
-letsCookBtn.addEventListener('click', displayMealOption);
-
-addARecipeTopBtn.addEventListener('click', function() {
-    addRecipeForm.classList.toggle('hidden');
+showAddRecipeFromBtn.addEventListener('click', function() {
+    addNewRecipeForm.classList.toggle('hidden');
 })
 
 clearMealBtn.addEventListener('click', function() {
     cookPotImg.classList.remove('hidden');
-    mealResults.classList.add('hidden');
+    showMealOption.classList.add('hidden');
     clearMealBtn.classList.add('hidden');
 })
 
 addNewRecipeBtn.addEventListener('click', addNewRecipe);
+letsCookBtn.addEventListener('click', displayMealOption);
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
-  }
+}
+
+function showMealBox() {
+    cookPotImg.classList.add('hidden');
+    clearMealBtn.classList.remove('hidden');
+    showMealOption.classList.remove('hidden');
+}
+
+function showCookPot() {
+    cookPotImg.classList.remove('hidden');
+    showMealOption.classList.add('hidden');
+    clearMealBtn.classList.add('hidden');
+}
+
 
 function generateDish() {
     let mealChoice = findMealValue();
-    if (mealChoice === 'sides') {
-        return sides[getRandomIndex(sides)];
-    } else if (mealChoice === 'main-dish') {
-        return mains[getRandomIndex(mains)];
-    } else if (mealChoice === 'dessert') {
-        return dessert[getRandomIndex(dessert)];
-    } else if (mealChoice === 'entire meal') {
-        entireMealOptions.push(mains[getRandomIndex(mains)]);
-        entireMealOptions.push(sides[getRandomIndex(sides)]);
-        entireMealOptions.push(dessert[getRandomIndex(dessert)]);
-        return entireMealOptions;
-    } else if (!mealChoice) {
-        alert('Pick something')
+    switch (mealChoice) {
+        case 'sides':
+            return sides[getRandomIndex(sides)];
+        case 'main-dish':
+            return mains[getRandomIndex(mains)];
+        case 'dessert':
+            return dessert[getRandomIndex(dessert)];
+        case 'entire meal':
+            entireMealOptions.push(mains[getRandomIndex(mains)]);
+            entireMealOptions.push(sides[getRandomIndex(sides)]);
+            entireMealOptions.push(dessert[getRandomIndex(dessert)]);
+            return entireMealOptions;
+        case undefined:
+            alert('Pick something')
     }
 }
 
@@ -111,26 +124,24 @@ function findMealValue () {
 }
 
 function displayMealOption() {
-    cookPotImg.classList.add('hidden');
-    clearMealBtn.classList.remove('hidden');
-    mealResults.classList.remove('hidden');
+    showMealBox();
     let mealOption = generateDish();
     if (Array.isArray(mealOption)) {
-        mealResults.innerHTML = `<article class="make-this"><h3 class="you-should-make">You should make: </h3>
+        showMealOption.innerHTML = `<article class="make-this"><h3 class="you-should-make">You should make: </h3>
         <h3>${mealOption[0]} with a side of ${mealOption[1]} and ${mealOption[2]} for dessert!</h3></article>`;
         entireMealOptions = [];
     } else if (!mealOption) {
         cookPotImg.classList.remove('hidden');
         clearMealBtn.classList.add('hidden');
     } else {
-        mealResults.innerHTML = `<article class="make-this"><h3 class="you-should-make">You should make: </h3>
+        showMealOption.innerHTML = `<article class="make-this"><h3 class="you-should-make">You should make: </h3>
         <h1>${mealOption}!</h1></article>`;
     }
 }
 
 function addNewRecipe() {
-    let mealTypeVal = mealTypeInput.value;
-    let recipeNameVal = recipeNameInput.value;
+    let mealTypeVal = inputMealType.value;
+    let recipeNameVal = inputRecipeName.value;
     let recipeName = recipeNameVal.charAt(0).toUpperCase() + recipeNameVal.slice(1);
     if (mealTypeVal === 'sides') {
         sides.push(recipeName);
@@ -139,9 +150,8 @@ function addNewRecipe() {
     } else if (mealTypeVal === 'dessert') {
         dessert.push(recipeName);
     }
-    cookPotImg.classList.add('hidden');
-    clearMealBtn.classList.remove('hidden');
-    mealResults.classList.remove('hidden');
-    mealResults.innerHTML = `<article class="add-this"><h3 class="you-added">You added: </h3>
+    showMealBox();
+    showMealOption.innerHTML = `<article class="add-this"><h3 class="you-added">You added: </h3>
     <h1>${recipeName} to ${mealTypeVal}!</h1></article>`;
+    console.log(sides);
 }
